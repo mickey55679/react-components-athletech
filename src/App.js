35f263cd95f3eb418c05d2f4064a6f0468c7340a
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-26 19:05:15 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
-function App() {
+function MyGallery(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gallery-container">
+      <div className="gallery-box">
+        <div className="gallery-item">
+          <img
+            src="https://cdn.pixabay.com/photo/2019/12/02/07/07/autumn-4667080_1280.jpg"
+            alt="Gallery Item 1"
+          />
+          <div className="overlay">Image 1</div>
+        </div>
+        <div className="gallery-item">
+          <img
+            src="https://cdn.pixabay.com/photo/2020/11/03/16/13/road-5710320_1280.jpg"
+            alt="Gallery Item 2"
+          />
+          <div className="overlay">Image 2</div>
+        </div>
+        <div className="gallery-item">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/10/06/22/54/forest-3728911_1280.jpg"
+            alt="Gallery Item 3"
+          />
+          <div className="overlay">Image 3</div>
+        </div>
+        <div className="gallery-item">
+          <img
+            src="https://cdn.pixabay.com/photo/2018/12/18/02/57/foliage-3881675_1280.jpg"
+            alt="Gallery Item 4"
+          />
+          <div className="overlay">Image 4</div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to the gallery</h1>
+      <MyGallery />
+    </div>
+  );
+}
+
