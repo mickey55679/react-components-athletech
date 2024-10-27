5f2984d5b23126b17d3352b51d09503d86b17616
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-27 18:56:20 -0500

    login form

---
+++ src/Components/LoginForm/LoginForm.js
@@ -0,0 +1,29 @@
+function LoginForm() {
+  return (
+    <div className="login-container">
+      <div className="wrapper">
+        <form action="">
+          <h1>Login</h1>
+          <div className="input-box">
+            <input type="text" placeholder="Username" required />
+          </div>
+          <div className="input-box">
+            <input type="password" placeholder="Password" required />
+          </div>
+          
+          <div className="remember-forgot">
+            <label><input type="checkbox" />Remember me</label>
+            <a href="#">Forgot password?</a>
+          </div>
+
+          <button type="submit">Login</button>
+          <div className="register-link">
+            <p>Don't have an account? <a href="#">Register</a></p>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+export default LoginForm;
