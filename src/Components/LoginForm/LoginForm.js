28ad7ae5838f3b8eb498b0fcc0778f6d4c4e9137
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-27 18:59:21 -0500

    added icon

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,3 +1,5 @@
+import { FaUser } from "react-icons/fa";
+
 function LoginForm() {
   return (
     <div className="login-container">
@@ -6,20 +8,25 @@ function LoginForm() {
           <h1>Login</h1>
           <div className="input-box">
             <input type="text" placeholder="Username" required />
+            <FaUser />
           </div>
           <div className="input-box">
             <input type="password" placeholder="Password" required />
           </div>
-          
+
           <div className="remember-forgot">
-            <label><input type="checkbox" />Remember me</label>
+            <label>
+              <input type="checkbox" />
+              Remember me
+            </label>
             <a href="#">Forgot password?</a>
           </div>
 
           <button type="submit">Login</button>
           <div className="register-link">
-            <p>Don't have an account? <a href="#">Register</a></p>
-
+            <p>
+              Don't have an account? <a href="#">Register</a>
+            </p>
           </div>
         </form>
       </div>
