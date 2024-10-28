2f25f8ab8c23074b657128733073d688db8160b4
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-27 19:04:52 -0500

    moved Gallery to own file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react";
-import MyGallery from './MyGallery';
+import MyGallery from './Components/Gallery/MyGallery';
 import LoginForm from './Components/LoginForm/LoginForm';
 
 
