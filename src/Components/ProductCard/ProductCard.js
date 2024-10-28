375f151c3b9b2e919dbfcafd5e0cfc5e7d4a93d3
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-28 12:52:41 -0500

    quotes adjusted for consistency

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -1,12 +1,19 @@
-import './ProductCard.css';
-import Data from './Data';
+import "./ProductCard.css";
+import Data from "./Data";
 
 function ProductCard() {
   return (
-  <div className='mainContainer'>
+  <div className="mainContainer">
     {
         Data.map(({id, image, title, description, price}) => {
-         
+         <div key={id} className="box">
+            <div className="card">
+                <div className="image">
+                    <img src={image} alt="" />
+
+                </div>
+            </div>
+         </div>
         })
     }
 
