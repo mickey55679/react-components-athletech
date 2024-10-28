775f087db255e2f34f470299f812e37b82366b15
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-28 12:46:59 -0500

    added images to data

--- src/Components/ProductCard/Data.js
+++ src/Components/ProductCard/Data.js
@@ -1,6 +1,11 @@
+import boots from '../Assets/cowboy-boots-4657844_1280-removebg-preview.png';
+import hat from '../Assets/cowboyhat.webp';
+import winterBoots from '../Assets/winterBoots.webp';
+
 const Data = [
   {
     id: 1,
+    image: boots,
     title: "cowboy boots",
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien felis dictum tempus commodo eu potenti velit sagittis. Eget sagittis primis praesent ut pellentesque vestibulum nostra.",
@@ -8,6 +13,7 @@ const Data = [
   },
   {
     id: 2,
+    image: hat,
     title: "hat",
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien felis dictum tempus commodo eu potenti velit sagittis. Eget sagittis primis praesent ut pellentesque vestibulum nostra.",
@@ -16,6 +22,7 @@ const Data = [
   {
     id: 3,
     title: "winter boots",
+    image: winterBoots,
     description:
       "Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien felis dictum tempus commodo eu potenti velit sagittis. Eget sagittis primis praesent ut pellentesque vestibulum nostra.",
     price: 60,
