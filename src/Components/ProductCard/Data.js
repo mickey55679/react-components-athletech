3723e530dcc12318de75a466dd0512843b77b3f5
Michaiah Bos <michaiahbos@yahoo.com>
2024-10-28 12:43:04 -0500

    added data for product cards

---
+++ src/Components/ProductCard/Data.js
@@ -0,0 +1,24 @@
+const Data = [
+  {
+    id: 1,
+    title: "cowboy boots",
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien felis dictum tempus commodo eu potenti velit sagittis. Eget sagittis primis praesent ut pellentesque vestibulum nostra.",
+    price: 290,
+  },
+  {
+    id: 2,
+    title: "hat",
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien felis dictum tempus commodo eu potenti velit sagittis. Eget sagittis primis praesent ut pellentesque vestibulum nostra.",
+    price: 60,
+  },
+  {
+    id: 3,
+    title: "winter boots",
+    description:
+      "Lorem ipsum odor amet, consectetuer adipiscing elit. Sapien felis dictum tempus commodo eu potenti velit sagittis. Eget sagittis primis praesent ut pellentesque vestibulum nostra.",
+    price: 60,
+  },
+];
+export default Data;
