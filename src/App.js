33d7af1c8165468e0b27f7d1218adbb453addb8f
Michaiah Bos <michaiahbos@yahoo.com>
2024-11-09 14:25:32 -0600

    put components all in index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from "react";
-import MyGallery from './Components/Gallery/MyGallery';
-import LoginForm from './Components/LoginForm/LoginForm';
-import ProductCard from './Components/ProductCard/ProductCard';
-import Checkout from './Components/Checkout/Checkout';
-import ShoppingCart from './Components/ShoppingCart/ShoppingCart'
+// import MyGallery from './Components/Gallery/MyGallery';
+// import LoginForm from './Components/LoginForm/LoginForm';
+// import ProductCard from './Components/ProductCard/ProductCard';
+// import Checkout from './Components/Checkout/Checkout';
+// import ShoppingCart from './Components/ShoppingCart/ShoppingCart'
+import {Checkout, LoginForm, MyGallery, ProductCard, ShoppingCart} from './Components/index'
 
 
 
---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import MyGallery from "./Gallery/MyGallery";
+import LoginForm from "./LoginForm/LoginForm";
+import ProductCard from "./ProductCard/ProductCard";
+import Checkout from "./Checkout/Checkout";
+import ShoppingCart from "./ShoppingCart/ShoppingCart";
+
+export { MyGallery, LoginForm, ProductCard, Checkout, ShoppingCart };
