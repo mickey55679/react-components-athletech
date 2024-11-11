898344be9ff80c5c59b3f7b3c03f6af4dc7b61c6
Michaiah Bos <michaiahbos@yahoo.com>
2024-11-11 12:40:34 -0600

    navbar structure

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,28 @@
 import React from "react";
 import "./Navbar.css";
+import myLogo from '../Assets/doglogo.webp'
 
 function Navbar() {
 
   return (
-   <>
-   </>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+          <li>
+            <a href="#"></a>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
