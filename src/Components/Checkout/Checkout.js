f5e651ab494d2851bc810a1c00640fcf04c3d50a
Michaiah Bos <michaiahbos@yahoo.com>
2024-11-10 23:31:18 -0600

    fixed import expots

--- src/Components/Checkout/Checkout.js
+++ src/Components/Checkout/Checkout.js
@@ -1,27 +1,58 @@
 import "./Checkout.css";
+import React from "react";
+import creditCardChip from "../Assets/chip.png";
+import visa from "../Assets/visa.png";
 
 function Checkout() {
   return (
     <>
       <div className="container">
+        <div className="card-container">
+          <div className="front">
+            <div className="image">
+              <img src={creditCardChip} alt="Credit Card Chip" />
+              <img src={visa} alt="Visa Logo" />
+            </div>
+            <div className="card-number-box">################</div>
+            <div className="flexbox">
+              <div className="box">
+                <span>card holder</span>
+                <div className="card-holder-name">full name</div>
+              </div>
+              <div className="box">
+                <span>expires</span>
+                <div className="expiration">
+                  <span className="exp-month">mm</span>
+                  <span className="exp-year">yy</span>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="back">
+            <div className="stripe"></div>
+            <div className="box">
+              <span>cvv</span>
+              <div className="cvv-box"></div>
+              <img src={visa} alt="Visa Logo" />
+            </div>
+          </div>
+        </div>
+
         <form action="">
           <div className="inputBox">
-            <span>Card Number</span>
-            <input
-              type="text"
-              maxLength="16"
-              className="card-number-input"
-            ></input>
+            <span>card number</span>
+            <input type="text" maxLength="16" className="card-number-input" />
           </div>
           <div className="inputBox">
-            <span>Card Holder</span>
-            <input type="text" className="card-holder-input"></input>
+            <span>card holder</span>
+            <input type="text" className="card-holder-input" />
           </div>
           <div className="flexbox">
             <div className="inputBox">
-              <span>experation mm</span>
+              <span>expiration mm</span>
               <select name="" id="" className="month-input">
-                <option value="month" select disabled>
+                <option value="month" disabled>
                   month
                 </option>
                 <option value="01">01</option>
@@ -39,9 +70,9 @@ function Checkout() {
               </select>
             </div>
             <div className="inputBox">
-              <span>experation yy</span>
+              <span>expiration yy</span>
               <select name="" id="" className="year-input">
-                <option value="year" select disabled>
+                <option value="year" disabled>
                   year
                 </option>
                 <option value="2021">2021</option>
@@ -50,20 +81,23 @@ function Checkout() {
                 <option value="2024">2024</option>
                 <option value="2025">2025</option>
                 <option value="2026">2026</option>
-                <option value="2027">2028</option>
+                <option value="2027">2027</option>
+                <option value="2028">2028</option>
                 <option value="2029">2029</option>
                 <option value="2030">2030</option>
               </select>
             </div>
-            <div className="inputbox">
+            <div className="inputBox">
               <span>cvv</span>
-              <input type="text" maxlength="4" class="cvv-input"></input>
+              <input type="text" maxLength="4" className="cvv-input" />
             </div>
           </div>
-          <input type="submit" value="submit" class="submit-btn" />
+          <input type="submit" value="submit" className="submit-btn" />
         </form>
       </div>
     </>
   );
 }
+
 export default Checkout;
+
