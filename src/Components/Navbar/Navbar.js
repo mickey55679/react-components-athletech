9b6a7bea982dbad555e3f53ac90b89791577624a
Michaiah Bos <michaiahbos@yahoo.com>
2024-11-11 13:00:01 -0600

    adding sidebar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,16 +3,34 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 import "./Navbar.css";
-const logo = require('../Assets/doglogo.webp');
+const logo = require('../Assets/aperture-1142967_1280.webp');
 
 function Navbar() {
 
   return (
     <>
       <nav>
+        <ul className="sidebar">
+          <li>
+            <a href="#">
+              <img src={logo} width="100%" />
+            </a>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
         <ul>
           <li>
-            <a href="#"><img src={logo} width="100%"/></a>
+            <a href="#">
+              <img src={logo} width="100%" />
+            </a>
           </li>
           <li>
             <a href="#">Home</a>
